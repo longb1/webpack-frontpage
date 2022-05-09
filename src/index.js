5c4936fdac8837fefab9911eae8cc6f55d875a68
Long <17272827+longb1@users.noreply.github.com>
2022-05-09 23:28:44 +0100

    add active class to home screen
    add active class to home container to make it appear by default, whilst hiding the other screens (or 'tabs')

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ const container=document.getElementById("container")
 
 function navBar(){
     const ulList= document.createElement("ul");
-    // ulList.classList.add("navBarUl");
     const navBarValues=["Home","About","Contact"];
     for(let i=0;i<3;i++){
         const liItem= document.createElement("li");
         const liContent=document.createElement("a");
 
-        liContent.href=`#${navBarValues[i]}`
+        liContent.href=`#${navBarValues[i]}`;
         liContent.textContent=navBarValues[i];
-        liContent.id=`nav${navBarValues[i]}`
+        liContent.id=`nav${navBarValues[i]}`;
         
         liItem.appendChild(liContent);
         ulList.appendChild(liItem);
@@ -23,19 +22,26 @@ function navBar(){
 container.appendChild(navBar());
 console.log(document.getElementById("navHome"))
 
+
+
 function homePage(){
     const homeContainer= document.createElement("div");
     const homeTitle=document.createElement("h1");
     const homePara=document.createElement("p");
-    homeTitle.textContent="home page";
+
+    homeContainer.classList.add("active"); //shows by default
     homeContainer.setAttribute("data-tab-content","");
+
+    homeTitle.textContent="home page";
     homePara.textContent="this is home page welcome"
 
     homeContainer.append(homeTitle,homePara)
     container.append(homeContainer)
     console.log(homeContainer)
+
+    return homeContainer
 }
-homePage()
+container.appendChild(homePage());
 
 
 function aboutPage(){
