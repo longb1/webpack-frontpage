de162f5597c9b06e0c6e946e43e66d34263b2ef5
Long <17272827+longb1@users.noreply.github.com>
2022-05-10 09:54:37 +0100

    finish tab functionality
    clicking on a navbar link now opens different tabs for the user to view.
    This was the main objective of the website therefore it is complete.

--- src/index.js
+++ src/index.js
@@ -10,33 +10,47 @@ function navBar(){
         const liItem= document.createElement("li");
         const liContent=document.createElement("a");
 
-        liContent.href=`#${navBarValues[i]}`
+        liContent.href=`#${navBarValues[i]}`;
         liContent.textContent=navBarValues[i];
-        liContent.id=`nav${navBarValues[i]}`
+        liContent.setAttribute("data-tab-target",`#nav${navBarValues[i]}`)
         
+        liContent.addEventListener("click",function(){
+            const allActiveContent=document.querySelectorAll("[data-tab-content]");
+            const contentSelector=document.querySelector(liContent.dataset.tabTarget);
+
+            allActiveContent.forEach(element => {
+                element.classList.remove("active")
+            });
+            
+            contentSelector.classList.add("active")
+        })
+
         liItem.appendChild(liContent);
         ulList.appendChild(liItem);
     }
 
     return ulList;
 }
-container.appendChild(navBar());
-console.log(document.getElementById("navHome"))
+
 
 function homePage(){
     const homeContainer= document.createElement("div");
     const homeTitle=document.createElement("h1");
     const homePara=document.createElement("p");
-    homeTitle.textContent="home page";
+    
     homeContainer.setAttribute("data-tab-content","");
+    homeContainer.id="navHome";
+
+    homeTitle.textContent="home page";
+
     homePara.textContent="this is home page welcome"
 
     homeContainer.append(homeTitle,homePara)
-    container.append(homeContainer)
-    console.log(homeContainer)
+    return homeContainer
 }
-homePage()
 
+container.appendChild(homePage());
+container.appendChild(navBar());
 
 function aboutPage(){
     
