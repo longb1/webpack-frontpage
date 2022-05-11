f7c7c7631568cf7cd9109765dbb1833c1c0af229
Long <17272827+longb1@users.noreply.github.com>
2022-05-11 08:10:24 +0100

    add font asset to site
    configure font loaders and styling for it to the site

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   devtool: 'inline-source-map',
   module: {
@@ -13,6 +12,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
