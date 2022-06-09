a71b8c1a78817d8b61e5c58ad5b4a5c2a6cd9175
Long <17272827+longb1@users.noreply.github.com>
2022-06-09 21:24:54 +0100

    implement react with babel and webpack
    successfully rendered a message in react.

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 
 class App extends Component {
     render (){
-        <div>
-            <h1>my react application wooo</h1>
-        </div>
+        return(
+            <div>
+                <h1>my react application wooo</h1>
+            </div>  
+        )
     }
 }
 
