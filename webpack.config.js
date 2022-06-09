fb6eab382684eb50db36edf8e48dee0b5d62e61a
Long <17272827+longb1@users.noreply.github.com>
2022-06-09 20:23:45 +0100

    remove font
    remove font, and also add htmlwebpackplugin to generate a html file in
    /dist (because clearning dist removes it)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin=require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean:true,
   },
   devtool: 'inline-source-map',
   module: {
@@ -12,10 +14,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
       },
       {
         test: /\.m?js$/,
@@ -31,4 +29,13 @@ module.exports = {
       }
     ],
   },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      title: 'Output Management',
+
+    }),
+
+  ],
 };
