7ce73ca45c366bba51a95cba805295cdb4354d69
long <17272827+longb1@users.noreply.github.com>
2022-06-14 14:34:18 +0100

    initial commit for new project
    create landing page for travel using react and props

---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NavBar(){
+
+
+    return(
+        <nav>
+            <h1>My travel journal</h1>
+        </nav>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './components/App.js';
 import FunctionComponent from "./components/functionComp.js";
 import "./style.css"
+import NavBar from "./components/navbar.js";
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 
 root.render(
    
     <div >
-        <App/>
+        <NavBar/>
         <FunctionComponent/>
     </div>   
    
