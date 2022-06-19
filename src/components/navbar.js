4879d349b739e609209783c479078214813df7ac
Long <17272827+longb1@users.noreply.github.com>
2022-06-19 09:29:24 +0100

    add slider for dark mode
    add slider to toggle dark mode

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 export default function NavBar(){
-
+    
 
     return(
         <nav>
             <h1>My travel journal</h1>
+
+            <label class="switch">
+                <input type="checkbox"/>
+                <span class="slider"></span>
+            </label>
         </nav>
     );
 }
